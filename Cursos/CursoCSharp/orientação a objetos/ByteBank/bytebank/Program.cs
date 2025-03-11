a5bb4f67a41771de7225adda63430fec607139cd
Lucas Wojcik <lucaswojcikk@gmail.com>
2025-03-10 21:47:12 -0300

    curso orientação objetos - 10-03-2025 21:47

---
+++ Cursos/CursoCSharp/orientação a objetos/ByteBank/bytebank/Program.cs
@@ -0,0 +1,88 @@
+﻿using bytebank.Contas;
+using bytebank.Titular;
+
+//ContaCorrente contaDoAndre = new ContaCorrente();
+//contaDoAndre.titular = "André Silva";
+//contaDoAndre.numero_agencia = 15;
+//contaDoAndre.conta = "1010-X";
+//contaDoAndre.saldo = 100;
+//Console.WriteLine("Saldo da conta do André =" + contaDoAndre.saldo);
+
+
+//ContaCorrente contaDoAndre2 = new ContaCorrente();
+//contaDoAndre2.titular = "André Silva";
+//contaDoAndre2.numero_agencia = 15;
+//contaDoAndre2.conta = "1010-X";
+//contaDoAndre2.saldo = 100;
+//Console.WriteLine("Saldo da conta do André =" + contaDoAndre2.saldo);
+
+//contaDoAndre = contaDoAndre2;
+
+//Console.WriteLine(contaDoAndre==contaDoAndre2);
+
+//ContaCorrente contaDaMaria = new ContaCorrente();
+//contaDaMaria.titular = "Maria Souza";
+//contaDaMaria.numero_agencia = 17;
+//contaDaMaria.conta = "1010-5";
+//contaDaMaria.saldo = 350;
+
+//Console.WriteLine("Saldo da conta da Maria =" + contaDaMaria.saldo);
+
+//contaDoAndre.Transferir(50, contaDaMaria);
+//Console.WriteLine("Saldo do André = " + contaDoAndre.saldo);
+//Console.WriteLine("Saldo da Maria = " + contaDaMaria.saldo);
+
+//ContaCorrente contaDoPedro = new ContaCorrente();
+//contaDoPedro.titular = "Pedro Silva";
+//Console.WriteLine(contaDoPedro.titular);
+//Console.WriteLine(contaDoPedro.saldo);
+//Console.WriteLine(contaDoPedro.numero_agencia);
+//Console.WriteLine(contaDoPedro.conta);
+
+//Cliente cliente = new Cliente();
+
+//cliente.nome = "Andre Silva";
+//cliente.cpf = "123456789";
+//cliente.profissao = "Analista";
+
+//ContaCorrente conta = new ContaCorrente();
+//conta.titular = cliente;
+//conta.conta = "1010-X";
+//conta.numero_agencia = 15;
+//conta.saldo = 100;
+
+//Console.WriteLine($"Titular = {conta.titular.nome}");
+//Console.WriteLine($"CPF = {conta.titular.cpf}");
+//Console.WriteLine($"Profissão = {conta.titular.profissao}");
+//Console.WriteLine($"Nº Conta = {conta.conta}");
+//Console.WriteLine($"Saldo = {conta.saldo}");
+//Console.WriteLine($"Nº Agência = {conta.numero_agencia}");
+
+
+//Cliente cliente2 = new Cliente();
+//ContaCorrente conta2 = new ContaCorrente();
+//conta2.titular = cliente2;
+
+//conta.titular.nome = "Andre Silva";
+//conta.titular.cpf = "123456789";
+//conta.titular.profissao = "Analista";
+
+//conta.conta = "1010-X";
+//conta.numero_agencia = 15;
+//conta.saldo = 100;
+
+//ContaCorrente conta3 = new ContaCorrente();
+//conta3.Numero_agencia = 18;
+//conta3.Conta = "1011-H";
+//conta3.SetSaldo(10);
+//Console.WriteLine(conta3.GetSaldo());
+//Console.WriteLine(conta3.Numero_agencia);
+//Console.WriteLine(conta3.Conta);
+
+
+ContaCorrente conta4 = new ContaCorrente(18, "1010-X");
+conta4.SetSaldo(500);
+conta4.Titular = new Cliente();
+
+Console.WriteLine(conta4.GetSaldo());
+Console.WriteLine(conta4.Numero_agencia);
---
+++ Cursos/CursoCSharp/orientação a objetos/SistemaMedico/Program.cs
@@ -0,0 +1,14 @@
+﻿
+using SistemaMedico;
+
+Pessoa novaPessoa = new Pessoa();
+
+novaPessoa.nome = "Lucas";
+novaPessoa.idade = 22;
+novaPessoa.altura = 1.72;
+novaPessoa.peso = 82.00;
+
+Console.WriteLine($"Nome: {novaPessoa.nome}");
+Console.WriteLine($"Idade: {novaPessoa.idade}");
+Console.WriteLine($"Altura: {novaPessoa.altura}");
+Console.WriteLine($"Peso: {novaPessoa.peso}");
