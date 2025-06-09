81968f61cfdc3ed205d049cb92b13aedb7cb2de7
LucasWojcik-Projevias <lucaswojcikk@gmail.com>
2025-06-09 17:22:52 -0300

    Documentando uma DLL

--- Cursos/CursoCSharp/criando_e_distribuindo_biliotecas_em_.net/bytebank_GeradorChavePix/GeradorPix.cs
+++ Cursos/CursoCSharp/criando_e_distribuindo_biliotecas_em_.net/bytebank_GeradorChavePix/GeradorPix.cs
@@ -7,13 +7,26 @@ using System.Threading.Tasks;
 
 namespace bytebank_GeradorChavePix
 {
+    /// <summary>
+    ///  Classe que gera chaves Pix usando o formato Guid.
+    /// </summary>
+    /// <returns></returns>
     public static class GeradorPix
     {
+        /// <summary>
+        /// Método que retorna uma chave aleatória PIX.
+        /// </summary>
+        /// <returns>Retorna uma chave PIX no formato String.</returns>
         public static string GetChavePix()
         {
             return Guid.NewGuid().ToString();
         }
 
+        /// <summary>
+        /// Método  que retorna uma lista aleatória de chaves PIX.
+        /// </summary>
+        /// <param name="numeroChaves"> Quantidade de chaves a serem geradas.</param>
+        /// <returns>Lista de strings de chaves Pix.</returns>
         public static List<string> GetChavesPix(int numeroChaves)
         {
             if (numeroChaves <= 0)
